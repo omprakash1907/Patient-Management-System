f1f31f28f466ffe329bb345445df94f7a2c8fca8
omprakash1907 <omprakashkjat19@gmail.com>
2024-11-01 23:37:53 +0530

    Added profile controller

--- server/src/config/stripeConfig.js
+++
@@ -1,5 +0,0 @@
-// config/stripeConfig.js
-const Stripe = require('stripe');
-const stripe = new Stripe('your_stripe_secret_key'); // Replace with your actual Stripe secret key
-
-module.exports = stripe;
