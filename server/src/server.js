5d12e3652c5b96563876d94282b9202ffbd91d50
Kashyap297 <kashyap29700@gmail.com>
2024-11-06 04:35:33 +0530

    front end access done to backend

--- server/src/server.js
+++ server/src/server.js
@@ -30,7 +30,7 @@ const PORT = Config.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: "http://localhost:3000", // Allow frontend origin
+  origin: "https://patient-management-system-gamma.vercel.app", // Allow frontend origin
   methods: ["GET", "POST", "PUT", "DELETE",'PATCH'],
   allowedHeaders: ["Authorization", "Content-Type"],
   credentials: true,
