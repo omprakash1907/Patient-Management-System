fec4fdf2b01e6f62292ebe9d980cac1e03284cec
omprakash1907 <omprakashkjat19@gmail.com>
2024-11-21 17:22:05 +0530

    updated url

--- server/src/server.js
+++ server/src/server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
-  origin: ['*'], // Allow frontend origin
+  origin: ['https://patient-management-system-gamma.vercel.app', 'http://localhost:3000'], // Allow frontend origin
   methods: ["GET", "POST", "PUT", "DELETE",'PATCH'],
   allowedHeaders: ["Authorization", "Content-Type"],
   credentials: true,
